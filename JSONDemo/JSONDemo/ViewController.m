962a749e481add17feb8f3d3aed71c3bacd82219
Sky-jay <lizeshaogaofeng@163.com>
2015-11-14 19:30:36 +0800

    Add query courier.

--- JSONDemo/JSONDemo/ViewController.m
+++ JSONDemo/JSONDemo/ViewController.m
@@ -12,15 +12,25 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *companyTextField;
 @property (weak, nonatomic) IBOutlet UITextField *waybillTextField;
-
+@property (weak, nonatomic) IBOutlet UILabel *companyShow;
+@property (weak, nonatomic) IBOutlet UILabel *waybillShow;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (nonatomic, strong) NSDictionary *dict;
 @end
 
 @implementation ViewController
 
 #define baseurl @"http://www.kuaidi100.com/query?"
+#define tempurl @"http://www.kuaidi100.com/query?type=zhongtong&postid=370771577426"
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _companyShow.backgroundColor = [UIColor whiteColor];
+    _waybillShow.backgroundColor = [UIColor whiteColor];
+    _companyShow.layer.cornerRadius = 5;
+    _companyShow.clipsToBounds = YES;
+    _waybillShow.layer.cornerRadius = 5;
+    _waybillShow.clipsToBounds = YES;
 }
 
 - (IBAction)queryAction:(UIButton *)sender {
@@ -28,17 +38,31 @@
     NSString *queryStr = [NSString stringWithFormat:@"type=%@&postid=%@",_companyTextField.text,_waybillTextField.text];
     NSString *string = [baseurl stringByAppendingString:queryStr];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    NSURLSessionDataTask *task = [session dataTaskWithURL:[NSURL URLWithString:string] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    NSURLSessionDataTask *task = [session dataTaskWithURL:[NSURL URLWithString:tempurl] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
         if (httpResponse.statusCode == 200) {
             id object = [data objectFromJSONData];
             if ([object isKindOfClass:[NSDictionary class]]) {
-                NSLog(@"--字典-->%@",object)
-                ;            }
+                NSLog(@"--字典-->%@",object);
+                _dict = object;
+            }
         }
     }];
     [task resume];
-    
+    [self infoShow];
+}
+
+- (void)infoShow
+{
+    NSLog(@"%@",_dict);
+    _companyShow.text = _dict[@"com"];
+    _waybillShow.text = _dict[@"nu"];
+    if (_dict[@"data"]) {
+        _textView.text = _dict[@"data"];
+    }else{
+        _textView.text = _dict[@"message"];
+
+    }
 }
 
 - (void)didReceiveMemoryWarning {
